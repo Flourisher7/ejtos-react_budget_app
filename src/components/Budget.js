7b214b397de46ceb41b1ea5a0ebabe237b1e15e7
Flourisher7 <christian.kazadimbamba@uq.net.au>
2023-03-02 07:47:55 -0500

    Completed tasks 3, 4 and 5

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,33 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, totalExpenses, currency } = useContext(AppContext);
+    const [updateBudget, setupBudget] = useState(budget);
+
+    const budgetChange = (event) => {
+        const newBudget = parseInt(event.target.value);
+        if (newBudget > 20000) {
+            alert('Budget cannot exceed the amount of' + currency+'20,000')
+            setupBudget(budget);
+            return;
+        } else if (newBudget < totalExpenses) {
+            alert('Budget cannot be less than' + currency+totalExpenses);
+            setupBudget(budget);
+            return;
+        } else {
+            setupBudget(newBudget)
+        }
+    }
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: {currency}<input required='required' 
+                                        id="number" 
+                                        step={10}
+                                        type="number" 
+                                        value={updateBudget}
+                                        onChange={budgetChange}></input></span>
         </div>
     );
 };
